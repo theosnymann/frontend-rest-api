012a31988389aa865ffc02b5d5be9ba4bdae2b0a
theosnymann <snymantheo03@gmail.com>
2022-07-04 23:10:22 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const helmet = require("helmet")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const path = require("path")
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@blog-website.buapb.mongodb.net/blog?retryWrites=true&w=majority`
+
+mongoose.connect(mongoString)
+
+mongoose.connection.on("error", function(error) {
+  if (process.env.NODE_ENV === "development") {
+    console.log(error)
+  }
+})
+
+mongoose.connection.on("open", function() {
+  console.log("Connected to MongoDB database.")
+})
+
+app.use(helmet())
+
+app.use(require("./routes/index.js"))
+
+app.use("/assets", express.static(path.join(__dirname, "..", "..", "assets")))
+
+app.listen(PORT, function () {
+  console.log(`Express app listening on port ${PORT}`)
+})
